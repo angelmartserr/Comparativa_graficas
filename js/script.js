0bdfb281199e279abf4b48ce8c7fbe7df9357886
angelmartserr <angelmartserr@gmail.com>
2022-02-23 21:06:12 +0100

    Acabada la web de comparativa de graficas

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function Desplegar(){
+    document.getElementById("table").style.display="block"
+    document.getElementById("desplegar").style.display="none"
+    document.getElementById("contraer").style.display="inline"
+}
+
+function Contraer(){
+    document.getElementById("table").style.display="none"
+    document.getElementById("desplegar").style.display="inline"
+    document.getElementById("contraer").style.display="none"
+}
